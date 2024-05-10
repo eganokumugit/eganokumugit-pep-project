4a3e30edf0622f3c175058e0a5c1696196cf9c51
Egan Okumu <okumueg@kean.edu>
2024-05-10 23:29:07 +0000

    AccountDAO v1

--- src/main/java/DAO/AccountDAO.java
+++ src/main/java/DAO/AccountDAO.java
@@ -1,11 +1,71 @@
 package DAO;
 
-import Model.Message;
+import Model.Account;
 import Util.ConnectionUtil;
 
 import java.sql.*;
-import java.util.ArrayList;
 public class AccountDAO 
 {
-    
+    // Check Username
+    public boolean userExists(String username)
+    {
+        Connection cnc = ConnectionUtil.getConnection();
+        try 
+        {
+            String sql = "SELECT username FROM account WHERE username=?;";
+            PreparedStatement ps = cnc.prepareStatement(sql);
+            ps.setString(1, username);
+
+            if(ps.executeUpdate() > 0)
+            {
+                return true;
+            }
+            
+        }catch (SQLException e) {
+            System.out.println("ERROR: " + e.getMessage());
+        }
+        return false;
+     }
+
+    // Register User
+     public Account registerAccount(Account acc)
+     {
+  
+        Connection cnc = ConnectionUtil.getConnection();
+        try 
+        {
+            String sql = "INSERT INTO account(username,password) VALUES(?,?);";
+            PreparedStatement ps = cnc.prepareStatement(sql);
+            ps.setString(1, acc.getUsername());
+            ps.setString(2, acc.getPassword());
+            ps.executeUpdate();
+            return acc;
+        }catch (SQLException e) {
+            System.out.println("ERROR: " + e.getMessage());
+        }
+        return null;
+     }
+    // Login User
+    public Account loginUser(String username, String password)
+    {
+       Connection cnc = ConnectionUtil.getConnection();
+       try 
+       {
+           String sql = "SELECT * FROM account WHERE username=? AND password=?;";
+           PreparedStatement ps = cnc.prepareStatement(sql);
+           ps.setString(1, username);
+           ps.setString(2, password);           
+           ResultSet rs = ps.executeQuery();
+           Account acc = new Account();
+           while(rs.next())
+           {
+               acc.setUsername(rs.getString("username"));
+               acc.setPassword(rs.getString("password"));
+           }
+           return acc;
+       }catch (SQLException e) {
+           System.out.println("ERROR: " + e.getMessage());
+       }
+       return null;
+    }
 }
